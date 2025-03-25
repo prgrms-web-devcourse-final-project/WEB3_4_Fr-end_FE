ce5fcdc165d3de53cae9ceda5de3cd03010e7409
chaeee1 <qocodus0212@gmail.com>
2025-03-25 15:57:04 +0900

    feat. commit test

---
+++ src/app/calendar/[id]/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CalendarPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Calendar Page</h1>
+      <p>This is the calendar page for a specific ID.</p>
+    </div>
+  );
+};
+
+export default CalendarPage;
