8800d15f1ae425fcc949a94deca9ba075168dc80
Yunho Yang <yangkles@naver.com>
2025-03-31 09:06:38 +0900

    feat: Footer 50% 구현

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -6,15 +6,9 @@ import React from "react";
 
 const Header: React.FC = () => {
   return (
-    <header className="w-[1440px] h-[125px] relative bg-white overflow-hidden mx-auto">
+    <header className="w-[1440px] h-[125px] relative bg-[#ffffff] overflow-hidden mx-auto">
       <div className="w-[1256px] px-2.5 inline-flex justify-start items-start gap-[637px]">
-        <Image
-          src="/logo/blue.png"
-          alt="Logo"
-          width={103}
-          height={42}
-          className="w-[103px] h-[42px]"
-        />
+        <Image src="/logo/blue.png" alt="Logo" width={103} height={42} />
         <div className="py-2.5 flex justify-start items-center gap-[27px]">
           <div className="w-[364px] h-[31px] px-4 py-[5px] bg-[#f2f2f2] rounded-[30px] outline-1 outline-offset-[-1px] outline-[#202020] flex justify-between items-center">
             <div className="w-[138px] flex justify-between items-end">
@@ -40,6 +34,20 @@ const Header: React.FC = () => {
               <Image src="/icons/user.svg" alt="user" width={20} height={20} />
             </div>
           </div>
+        </div>
+      </div>
+      <div className="w-[1256px] h-[52px] px-2.5 inline-flex justify-start items-center gap-[30px]">
+        <div className="justify-start text-[#1a1a1a] text-base font-semibold font-['Pretendard']">
+          숙소 예약
+        </div>
+        <div className="justify-start text-[#1a1a1a] text-base font-semibold font-['Pretendard']">
+          Schedule
+        </div>
+        <div className="justify-start text-[#1a1a1a] text-base font-semibold font-['Pretendard']">
+          메이트 찾기
+        </div>
+        <div className="justify-start text-[#1a1a1a] text-base font-semibold font-['Pretendard']">
+          여행게시판
         </div>
       </div>
     </header>
