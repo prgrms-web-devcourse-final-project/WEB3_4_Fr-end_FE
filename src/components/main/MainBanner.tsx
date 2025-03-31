dc0dc5fc26101083890bf264fa7febe804fef277
chaeee1 <qocodus0212@gmail.com>
2025-03-31 19:01:44 +0900

    feat. 배너수정

--- src/components/main/MainBanner.tsx
+++ src/components/main/MainBanner.tsx
@@ -1,17 +1,11 @@
-import Image from 'next/image';
+
 import React from 'react';
 
 export default function MainBanner() {
   return (
     <div className="relative">
       <div className="fixed top-0 left-0 w-screen h-[700px] z-[-1]">
-        <Image
-          src="/main/banner.jpg"
-          alt="hero image"
-          fill
-          className="object-cover"
-          priority
-        />
+       
       </div>
       <div className="h-[1000px]" />
     </div>
