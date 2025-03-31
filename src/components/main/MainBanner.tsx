29447a12ada3b97942a2849169b60e9ee530b6e6
chaeee1 <qocodus0212@gmail.com>
2025-03-31 23:09:30 +0900

    feat. 메인페이지 숙소예약섹션 완성

--- src/components/main/MainBanner.tsx
+++ src/components/main/MainBanner.tsx
@@ -1,13 +1,20 @@
-
 import React from 'react';
 
 export default function MainBanner() {
   return (
-    <div className="relative">
-      <div className="fixed top-0 left-0 w-screen h-[700px] z-[-1]">
-       
-      </div>
-      <div className="h-[1000px]" />
+    <div className="relative ">
+     <div className="relative text-customGray-100 whitespace-pre-line font-paperlogy text-5xl
+     leading-14 top-32 pl-24">
+  {`여행을 함께할
+    메이트부터 일정까지 한 곳에서`}
+     </div>
+
+     <div className="relative text-customGray-100 whitespace-pre-line top-42 pl-24
+     text-xl">
+  {`여행을 함께할 메이트를 찾는 것부터, 여행 일정까지 모든 기능을 한 곳에서 제공합니다. 
+새로운 여행 친구를 만나고, 함께 일정을 계획하며 더 즐거운 여행을 준비해보세요.`}
+     </div>
+      <div className="h-[290px]" />
     </div>
   );
 }
--- src/components/main/MainLodging.tsx
+++ src/components/main/MainLodging.tsx
@@ -1,9 +1,69 @@
 import React from 'react'
+import Image from 'next/image';
+import { Button } from '../ui/button';
 
 export default function MainLodging() {
   return (
     <div>
-      
+      <div className='relative'>
+        <p className='font-paperlogy text-4xl pt-16'>숙소 예약하기</p>
+        <div className='flex justify-between'>
+        <p className='text-lg pt-2 font-medium'>국내여행 숙소 모두 여기서, 숙소 찾고 결제까지 Planit에서 한번에!</p>
+        <Button className='mr-2 mb-4 rounded-2xl w-28 shadow-customBlack-400'>전체보기</Button>
+        </div>
+        <div className="flex w-full gap-14 ">
+  <div className="pt-4 flex-grow-[5] relative h-[418px] transition-transform duration-300 ease-in-out hover:scale-105">
+    <Image 
+      src='/main/lodging1.jpg' 
+      alt='mainlodging' 
+      fill
+      className='rounded-2xl object-cover w-full'
+    />
+        <div className="absolute inset-0 bg-gradient-to-t from-black/90 via-black/40 to-transparent rounded-2xl"></div>
+        <p className="absolute top-[290px] right-[130px] text-white text-5xl font-bold">
+      숙소 찾아보기
+    </p>
+    <p className="absolute top-[350px] right-[130px] text-white text-xl font-bold">
+    호텔, 모텔, 펜션, 풀빌라, 카라반 모두 한곳에서!
+    </p>
+    <div className="absolute top-[310px] right-[50px] flex items-center space-x-2">
+    <Image 
+      src='/svg/arrow.svg' 
+      alt='arrow icon'
+      width={48}
+      height={48}
+      className="text-white"
+    />
+    </div>
+  </div>
+  
+  <div className="pt-4 flex-grow-[3] relative transition-transform duration-300 ease-in-out hover:scale-105">
+    <Image 
+      src='/main/lodging2.jpg' 
+      alt='mainlodging' 
+      fill
+      className='rounded-2xl object-cover w-full h-full'
+    />
+    <div className="absolute inset-0 bg-gradient-to-t from-black/90 via-black/40 to-transparent rounded-2xl"></div>
+    <p className="absolute top-[290px] right-[130px] text-white text-5xl font-bold">
+    Planit 추천 숙소
+    </p>
+    <p className="absolute top-[350px] right-[130px] text-white text-xl font-bold">
+    Planit이 추천하는 인기 숙소! 결제까지 한번에~
+    </p>
+    <div className="absolute top-[310px] right-[50px] flex items-center space-x-2">
+    <Image 
+      src='/svg/arrow.svg' 
+      alt='arrow icon'
+      width={48}
+      height={48}
+      className="text-white"
+    />
+    </div>
+  </div>
+</div>
+      </div>
+      <div className='h-20'></div>
     </div>
   )
 }
