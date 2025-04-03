e597906657e17bbc49be23d2f90cbd8284ea704a
chaeee1 <qocodus0212@gmail.com>
2025-04-03 16:31:10 +0900

    feat. 캘린더페이지 트리 네비게이션 바, 풀캘린더 설치

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -1,11 +1,13 @@
+import CalendarNavTree from "@/components/calendar/CalendarNavTree"
+import FCalendar from "@/components/calendar/FCalendar"
 
 export default function CalendarPage() {
   return (
     <div>
-      {/* 캘린더 내비바 */}
-      <div className="relative border border-customGray-100 mb-2 h-[700px] w-[343px] shadow-custom-figma">
-      <p className="relative font-semibold text-xl pl-10 pt-8">+ New calendar</p>
-      </div>
+      <div className="flex h-screen">
+   <CalendarNavTree />
+   <FCalendar/>
+   </div>
     </div>
   )
 }
