b8a104a7d3327a2acf8a8f890147c8af6d13bd53
Yunho Yang <yangkles@naver.com>
2025-04-03 16:21:10 +0900

    feat: myPage-ScheduleManager

---
+++ src/components/myPage/schedule/ScheduleCard.tsx
@@ -0,0 +1,83 @@
+"use client";
+
+import { CalendarMate, CalendarData } from "@/types/ScheuduleData";
+import { useState } from "react";
+
+interface CalendarProps {
+  calendars: CalendarData[];
+}
+
+const badgeColors = [
+  "bg-red-400",
+  "bg-orange-400",
+  "bg-yellow-400",
+  "bg-green-400",
+  "bg-teal-400",
+  "bg-blue-400",
+  "bg-indigo-400",
+  "bg-purple-400",
+  "bg-pink-400",
+];
+
+export default function ScheduleCard({ calendars }: CalendarProps) {
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const commentPerPage = 3;
+
+  const currentCalendar = calendars.slice(0, currentPage * commentPerPage);
+  const totalCalendar = Math.ceil(calendars.length / commentPerPage);
+
+  return (
+    <div className="flex flex-col items-center">
+      {currentCalendar.map((calendar) => {
+        return (
+          <div
+            key={calendar.id}
+            className="w-[726px] min-h-[115px] outline outline-customGray-300 rounded-[4px] p-[15px] mb-[15px]"
+          >
+            <div className="flex justify-between mb-[5px]">
+              <div className="font-pretendard font-bold text-[20px] text-customBlack-100">
+                {calendar.name}
+              </div>
+              <div className="text-red-800 font-pretendard font-semibold text-[10px] hover:text-red-300 cursor-pointer">
+                캘린더 삭제
+              </div>
+            </div>
+            <div className="font-pretendard font-normal text-[13px] text-customGray-500 mb-[15px]">
+              {calendar.description}
+            </div>
+            <div className="flex items-center gap-[10px]">
+              <div className="font-pretendard font-semibold text-[13px]">
+                Mate&nbsp;:&nbsp;&nbsp;
+              </div>
+              {calendar.mates.map((mate, i) => {
+                const randomColor =
+                  badgeColors[Math.floor(Math.random() * badgeColors.length)];
+
+                return (
+                  <div
+                    key={i}
+                    className={`min-w-[50px] h-[16px] px-[6px] py-[2px] ${randomColor} rounded-full flex justify-center items-center`}
+                  >
+                    <div className="text-white font-pretendard font-semibold text-[9px]">
+                      {mate.userNickname}
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        );
+      })}
+      {currentPage < totalCalendar && (
+        <div
+          onClick={() => setCurrentPage((prev) => prev + 1)}
+          className="cursor-pointer w-[156px] h-10 px-[61px] py-3 bg-customBlack-300 rounded-lg inline-flex justify-center items-center gap-2.5 mt-[26px] mb-[113px] hover:bg-customGray-600"
+        >
+          <div className="text-white text-[13px] font-semibold font-['Pretendard']">
+            더보기
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
--- src/components/myPage/schedule/ScheduleManager.tsx
+++ src/components/myPage/schedule/ScheduleManager.tsx
@@ -1,10 +1,14 @@
+import ScheduleCard from "./ScheduleCard";
+import { calendarDummyData } from "@/dummyData/CalendarDummyData";
+
 export default function ScheduleManager() {
   return (
-    <div className="w-[726px] min-h-[927px] flex">
+    <div className="w-[726px] min-h-[927px] flex flex-col">
       <div className="font-pretendard font-bold text-[28px] text-customBlack-400">
         내 캘린더 관리
       </div>
-      
+      <div className="w-[726px] h-[1px] bg-customGray-400 mt-[50px] mb-[20px]" />
+      <ScheduleCard calendars={calendarDummyData}/>
     </div>
   );
 }
