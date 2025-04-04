64b84a82227252d15f0b508e3219b647b22ba2ce
leejihwan99 <lbwlimes@gmail.com>
2025-04-04 17:25:00 +0900

    feat: 숙소 메인페이지 스와이퍼로 작업 및 더미데이터 연결

--- src/components/reservation/ReservationMateSection.tsx
+++ src/components/reservation/ReservationMateSection.tsx
@@ -1,73 +1,52 @@
+"use client";
+
 import Image from "next/image";
 import MateCard from "./MateCard";
 
-const mateCardsData = [
-  {
-    imageSrc: "/reservationImg/mateCardTest.webp",
-    title: "5월에 같이 제주도 가실 분",
-    nickName: "여행자1",
-    startDate: "25.05.01",
-    endDate: "25.05.02",
-    location: "제주도",
-  },
-  {
-    imageSrc: "/reservationImg/mateCardTest.webp",
-    title: "4월에 같이 서울을 여행할 분",
-    nickName: "여행자2",
-    startDate: "25.04.10",
-    endDate: "25.04.12",
-    location: "서울",
-  },
-  {
-    imageSrc: "/reservationImg/mateCardTest.webp",
-    title: "6월에 부산 바다를 함께 갈 친구",
-    nickName: "여행자3",
-    startDate: "25.06.15",
-    endDate: "25.06.16",
-    location: "부산",
-  },
-  {
-    imageSrc: "/reservationImg/mateCardTest.webp",
-    title: "강원도에서 힐링할 동반자 구합니다",
-    nickName: "여행자4",
-    startDate: "25.07.01",
-    endDate: "25.07.03",
-    location: "강원도",
-  },
-];
+import { Swiper, SwiperSlide } from "swiper/react";
+import { FreeMode, Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
 
-export default function ReservationMateSection() {
+import type { MateCardData } from "@/types/MateCardData";
+
+export default function ReservationMateSection({
+  cards,
+}: {
+  cards: MateCardData[];
+}) {
   return (
-    <>
-      <section className="flex gap-10 w-full h-90 my-20">
-        <div className="w-96">
-          <div className="font-paperlogy text-6xl">#같이 가요</div>
-          <div className="text-2xl font-semibold mt-4 mb-15">
-            플래닛으로 구하는 여행메이트
-          </div>
-          <div className="ml-12 relative w-84 h-50">
-            <Image
-              src={"/svg/traveling.svg"}
-              alt="여행svg"
-              fill
-              objectFit="cover"
-            />
-          </div>
+    <section className="flex gap-10 w-full h-90 my-20">
+      <div className="w-96">
+        <div className="font-paperlogy text-6xl">#같이 가요</div>
+        <div className="text-2xl font-semibold mt-4 mb-15">
+          플래닛으로 구하는 여행메이트
         </div>
-        <div className="w-full flex gap-4 bg-secondary">
-          {/* {mateCardsData.map((mate, index) => (
-            <MateCard
-              key={index}
-              imageSrc={mate.imageSrc}
-              title={mate.title}
-              nickName={mate.nickName}
-              startDate={mate.startDate}
-              endDate={mate.endDate}
-              location={mate.location}
-            />
-          ))} */}
+        <div className="ml-12 relative w-84 h-50">
+          <Image
+            src={"/svg/traveling.svg"}
+            alt="여행svg"
+            fill
+            objectFit="cover"
+          />
         </div>
-      </section>
-    </>
+      </div>
+      <div className="w-full flex gap-4 overflow-auto">
+        <Swiper
+          slidesPerView={3.3}
+          spaceBetween={16}
+          freeMode={true}
+          modules={[FreeMode, Pagination]}
+          style={{ overflow: "hidden" }}
+        >
+          {cards.map((card, index) => (
+            <SwiperSlide key={index}>
+              <MateCard key={card.id} data={card} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </section>
   );
 }
