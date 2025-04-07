5dfb54b1801442be55b00e6323b81b9f9362bf18
chaeee1 <qocodus0212@gmail.com>
2025-04-07 10:35:33 +0900

    feat. 브랜치 바꾸기용 커밋

--- src/app/calendar/[id]/page.tsx
+++ src/app/calendar/[id]/page.tsx
@@ -1,9 +1,15 @@
+'use client';
+import FCalendar from "@/components/calendar/FCalendar"
+import dynamic from 'next/dynamic';
+const CalendarNav = dynamic(() => import('@/components/calendar/CalendarNav'), { ssr: false });
 
-export default function page() {
-
+export default function CalendarPage() {
   return (
     <div>
-      
+      <div className="flex h-screen">
+   <CalendarNav/>
+   <FCalendar/>
+   </div>
     </div>
   )
 }
