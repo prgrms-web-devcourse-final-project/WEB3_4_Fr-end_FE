2af9d05440097f5f13aaadbf1ea3eb1c782b7f92
chaeee1 <qocodus0212@gmail.com>
2025-04-09 13:55:45 +0900

    feat. api 요청 수정

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,13 +1,22 @@
-// lib/axios.ts
 import axios from 'axios';
 
-export const API_URL = 'http://api.sete.kr:8080/api';
+const API_URL = 'http://api.sete.kr:8080/api/calendar';
 
-const axiosInstance = axios.create({
-  baseURL: API_URL,
-  headers: {
-    'Content-Type': 'application/json',
-  },
-});
+export const fetchCalendars = async () => {
+  const response = await axios.get(API_URL);
+  return response.data;
+};
 
-export default axiosInstance;
+export const createCalendar = async (label: string) => {
+  const now = new Date().toISOString();
+  const body = {
+    calendarTitle: label,
+    startDate: now,
+    endDate: now,
+    time: now,
+    alertTime: now,
+    note: ''
+  };
+  const response = await axios.post(API_URL, body);
+  return response.data;
+};
