0c4525035c47b7abf5ffa69811b7dcebd13e182f
Joonsenia <128702580+Joon-Leo@users.noreply.github.com>
2025-04-12 01:19:36 +0900

    fix: Zustand 수정

--- src/apis/mateBoard/postApplication.ts
+++ src/apis/mateBoard/postApplication.ts
@@ -3,8 +3,10 @@ import apiClient from "@/config/axiosConfig";
 export const postApplication = async (postId: number) => {
   try {
     console.log("동행신청 postId:", postId);
-    const response = await apiClient.post(`/mate-board/applications/${postId}`);
-    return response.data;
+    const response = await apiClient.post(
+      `/api/v1/mate-board/applications/${postId}`
+    );
+    return response;
   } catch (error) {
     console.error("동행 신청 POST 에러:", error);
     throw error;
